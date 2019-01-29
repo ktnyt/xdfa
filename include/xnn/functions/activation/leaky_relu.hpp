9db404ac79bd16c366af9c46a1c0b2096ce8c6a2
Kotone Itaya <kotone@sfc.keio.ac.jp>
2019-01-30 00:07:57 +0900

    Add Leaky ReLU

---
+++ include/xnn/functions/activation/leaky_relu.hpp
@@ -0,0 +1,54 @@
+#ifndef __XNN_FUNCTIONS_ACTIVATION_LEAKY_RELU_HPP__
+#define __XNN_FUNCTIONS_ACTIVATION_LEAKY_RELU_HPP__
+
+#include "xnn/function.hpp"
+
+#include "xtensor/xarray.hpp"
+#include "xtensor/xindex_view.hpp"
+#include "xtensor/xmath.hpp"
+
+#include "doctest.h"
+
+namespace xnn {
+namespace functions {
+namespace activation {
+
+class LeakyReLU : public Function<float> {
+ public:
+  LeakyReLU(float slope = 0.2) : slope(slope) {}
+
+  xt::xarray<float> operator(xt::xarray<float> x) {
+    xt::filtration(x, x < 0) *= slope;
+  }
+
+ private:
+  float slope;
+};
+
+class LeakyReLUGrad : public Function<float> {
+ public:
+  LeakyReLU(float slope = 0.2) : slope(slope) {}
+
+  xt::xarray<float> operator(xt::xarray<float> x) {
+    xt::filtration(x, x > 0) = 1.0;
+    xt::filtration(x, x <= 0) = slope;
+    return x;
+  }
+
+ private:
+  float slope;
+};
+
+xt::xarray<float> leaky_relu(xt::xarray<float> x, float slope = 0.2) {
+  return LeakyReLU(slope)(x);
+}
+
+xt::xarray<float> leaky_relu_grad(xt::xarray<float> x, float slope = 0.2) {
+  return LeakyReLUGrad(slope)(x);
+}
+
+}  // namespace activation
+}  // namespace functions
+}  // namespace xnn
+
+#endif  // __XNN_FUNCTIONS_ACTIVATION_LEAKY_RELU_HPP__
---
+++ include/xnn/layers/activation/leaky_relu.hpp
@@ -0,0 +1,45 @@
+#ifndef __XNN_LAYERS_ACTIVATION_LEAKY_RELU_HPP__
+#define __XNN_LAYERS_ACTIVATION_LEAKY_RELU_HPP__
+
+#include "xnn/functions/activation/leaky_relu.hpp"
+#include "xnn/layer.hpp"
+
+#include "xtensor/xarray.hpp"
+#include "xtensor/xmath.hpp"
+
+#include <queue>
+
+namespace xnn {
+namespace layers {
+namespace activation {
+
+class LeakyReLU final : public Layer<float> {
+  class Impl final : public Layer<float>::Impl {
+   public:
+    Impl(float slope) : slope(slope) {}
+
+    xt::xarray<float> forward(xt::xarray<float> x) override {
+      queue.push(functions::activation::leaky_relu(x, slope));
+      return queue.front();
+    }
+
+    xt::xarray<float> backward(xt::xarray<float> d) override {
+      xt::xarray<float> y = queue.front();
+      queue.pop();
+      return d * functions::activation::leaky_relu_grad(y, slope);
+    }
+
+   private:
+    float slope;
+    std::queue<xt::xarray<float>> queue;
+  };
+
+ public:
+  LeakyReLU(float slope = 0.2) : Layer<float>(std::make_shared<Impl>(slope)) {}
+};
+
+}  // namespace activation
+}  // namespace layers
+}  // namespace xnn
+
+#endif  // __XNN_LAYERS_ACTIVATION_LEAKY_RELU_HPP__
